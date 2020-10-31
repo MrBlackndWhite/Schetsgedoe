8e06c7243fd4f0c3ce89cd35178a5ae02a35ea96
Dinesh Tikai <dineshtikai@gmail.com>
2020-10-31 19:46:00 +0100

    Initial Project, again.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace plswork
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new Hoofdscherm());
+        }
+    }
+}
